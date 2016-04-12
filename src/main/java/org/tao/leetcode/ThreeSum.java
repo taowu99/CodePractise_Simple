f4286d2759f4464d8e1bc12477e3b07b5a71cd4f
Tao Wu <taowu99@gmail.com>
2016-04-12 12:01:50 -0400

    three sum

---
+++ src/main/java/org/tao/leetcode/ThreeSum.java
@@ -0,0 +1,36 @@
+package org.tao.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+	public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> res = new ArrayList();
+        int l = nums.length;
+        for (int i=0; i< l-2; ++i) {
+        	int a=i+1, e=l-1;
+        	while (a<e) {
+        		final int sum = nums[i]+nums[a]+nums[e];
+        		if (sum==0) {
+        			res.add(Arrays.asList(nums[i],nums[a],nums[e]));
+        			do {
+        				++a;
+        			}while (a<l && nums[a]==nums[a-1]);
+        			do {
+        				--e;
+        			}while (e>=0 && nums[e]==nums[e+1]);
+        		}
+        		else if (sum<0)
+        			++a;
+        		else
+        			--e;
+        	}
+        	while (i<l-2 && nums[i]==nums[i+1])
+        		++i;
+        }
+        
+        return res;
+    }
+}
---
+++ src/test/java/org/tao/leetcode/ThreeSumTest.java
@@ -0,0 +1,11 @@
+package org.tao.leetcode;
+
+import org.junit.Test;
+
+public class ThreeSumTest {
+	@Test
+	public void testThreeSum() {
+		ThreeSum ts = new ThreeSum();
+		System.out.println(ts.threeSum(new int[]{1,-1,-1,0}));
+	}
+}
