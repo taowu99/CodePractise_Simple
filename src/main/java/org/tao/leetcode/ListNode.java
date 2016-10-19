254d6e556fba270fcd710e57def43ec2b5d943ed
Tao Wu <taowu99@gmail.com>
2016-10-19 09:31:47 -0400

    Node for LinkedList

---
+++ src/main/java/org/tao/leetcode/ListNode.java
@@ -0,0 +1,13 @@
+package org.tao.leetcode;
+
+/**
+ * Created by Tao Wu on 10/19/2016.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
