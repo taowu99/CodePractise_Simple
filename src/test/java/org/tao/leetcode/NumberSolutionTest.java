06dfff50e05dc1fb7a74f20cd5d605505c754328
Tao Wu <taowu99@gmail.com>
2017-02-21 13:46:51 -0500

    Leetcode 69 sqrt

--- src/test/java/org/tao/leetcode/NumberSolutionTest.java
+++ src/test/java/org/tao/leetcode/NumberSolutionTest.java
@@ -19,4 +19,13 @@ public class NumberSolutionTest {
         Assert.assertEquals("-0.1(6)", solution.fractionToDecimal(-1,6));
         Assert.assertEquals("0.(01)", solution.fractionToDecimal(1,99));
     }
+
+    @Test
+    public void testMySqrt() {
+        NumberSolution solution = new NumberSolution();
+        System.out.println("sqrt("+2147483647+")="+solution.mySqrt(2147483647));
+        System.out.println("sqrt("+2147395599+")="+solution.mySqrt(2147395599));
+        for (int i=0; i<10; ++i)
+            System.out.println("sqrt("+i+")="+solution.mySqrt(i));
+    }
 }
